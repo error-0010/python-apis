c8f11356378f5640be334f552c1d6631c9ef7ad9
error-0010 <nicolas.riquelme.sos@gmail.com>
2023-02-11 00:17:51 -0300

    segunda prueba

--- primeraApi.py
+++ primeraApi.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+app =from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/my-first-api")
-def hola():
-    return {"hola apis mundo!"}
+def hola(name: str):
+    return {"hola apis mundo!" + name + "!"}
